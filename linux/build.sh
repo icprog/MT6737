18a8c668314698bca19cd90c16efe73e93ff9c26
qitas <qitas@qitas.cn>
2019-07-22 10:28:14 +0800

    fix build

--- linux/build.sh
+++ linux/build.sh
@@ -8,6 +8,7 @@ set -e
 
 export ROOT=`pwd`
 SCRIPTS=$ROOT/scripts
+
 export BOOT_PATH
 export ROOTFS_PATH
 export UBOOT_PATH
@@ -27,17 +28,23 @@ fi
 
 function get_toolchain()
 { 
-    	if [ ! -d $ROOT/toolchain/gcc-arm-eabi -a ! -d $ROOT/arm-eabi ]; then
-		cd $ROOT
-		git clone --depth=1 https://github.com/sochub/arm-eabi.git
-    	fi
-    	if [ -d $ROOT/arm-eabi -a ! -d $ROOT/toolchain/gcc-arm-eabi ]; then
-        	mv $ROOT/arm-eabi $ROOT/toolchain
-    	fi
+	if [ ! -d $ROOT/toolchain ]; then
+			cd $ROOT
+			mkdir  toolchain
+	fi
+	if [ -d $ROOT/toolchain ]; then
+		cd $ROOT/toolchain
+		count=`ls $*|wc -w`
+		if [ "$count" <"1" ]; then
+			 git clone --depth=1 https://github.com/sochub/arm-eabi.git   arm-eabi-4.8
+		else
+			cd  arm-eabi-4.8
+			git pull
+		fi
+	fi
 }
 
-git_configure
-get_toolchain
+
 
 root_check()
 {
@@ -128,9 +135,6 @@ Update_check()
     	3>&1 1>&2 2>&3)
 }
 
-if [ ! -d $ROOT/output ]; then
-    mkdir -p $ROOT/output
-fi
 
 
 ##########################################
@@ -159,26 +163,11 @@ done
 
 echo $PASSWD | sudo ls &> /dev/null 2>&1
 
-## Check cross tools
-if [ ! -d $ROOT/toolchain/gcc-linaro-aarch -o ! -d $ROOT/toolchain/gcc-linaro-aarch/gcc-linaro/arm-linux-gnueabi ]; then
-	cd $SCRIPTS
-#	./install_toolchain.sh
-	cd -
-fi
 
 if [ ! -d $ROOT/output ]; then
     mkdir -p $ROOT/output
 fi
 
-## prepare development tools
-if [ ! -f $ROOT/output/.tmp_toolchain ]; then
-	cd $SCRIPTS
-	sudo ./Prepare_toolchain.sh
-	touch $ROOT/output/.tmp_toolchain
-	cd -
-fi
-
-MENUSTR="Pls select build option"
 
 OPTION=$(whiptail --title "MT6737 Linux Build System" \
 	--menu "$MENUSTR" 20 60 12 --cancel-button Finish --ok-button Select \
@@ -187,6 +176,9 @@ OPTION=$(whiptail --title "MT6737 Linux Build System" \
 	"2"   "Build Linux" \
 	"3"   "Install Image into EMMC" \
 	3>&1 1>&2 2>&3)
+
+git_configure
+get_toolchain
 
 if [ $OPTION = "0" -o $OPTION = "0" ]; then
 	sudo echo ""
